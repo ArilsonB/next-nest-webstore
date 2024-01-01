df15df58b161be5dfcfd98385e27291d340c8f7f
Arilson B. Ferreira <arilsondeveloper@gmail.com>
2024-01-01 20:40:32 -0300

    updated packages

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,13 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ logger: false }),
   );
-  await app.listen(3000);
-}
+
+  await app.listen(3000, '0.0.0.0');
+};
+
 bootstrap();
